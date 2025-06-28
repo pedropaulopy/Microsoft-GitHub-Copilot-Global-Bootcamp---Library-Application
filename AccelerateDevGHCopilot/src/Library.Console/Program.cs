3ce8b543c8844bd58cd12e1078798e6de3c81a2c
pedro_paulo <pedropaulodecarlo@gmail.com>
2025-06-28 15:30:49 -0300

    first commit

---
+++ AccelerateDevGHCopilot/src/Library.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Library.ApplicationCore;
+using Microsoft.Extensions.Configuration;
+using Library.Infrastructure.Data;
+
+var services = new ServiceCollection();
+
+services.AddSingleton<JsonData>();
+services.AddSingleton<ConsoleApp>();
+
+var configuration = new ConfigurationBuilder()
+.SetBasePath(Directory.GetCurrentDirectory())
+.AddJsonFile("appSettings.json")
+.Build();
+
+services.AddSingleton<IConfiguration>(configuration);
+
+services.AddScoped<IPatronRepository, JsonPatronRepository>();
+services.AddScoped<ILoanRepository, JsonLoanRepository>();
+services.AddScoped<ILoanService, LoanService>();
+services.AddScoped<IPatronService, PatronService>();
+
+services.AddSingleton<JsonData>();
+services.AddSingleton<ConsoleApp>();
+
+var servicesProvider = services.BuildServiceProvider();
+
+var consoleApp = servicesProvider.GetRequiredService<ConsoleApp>();
+consoleApp.Run().Wait();
